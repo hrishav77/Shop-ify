60f2ad0ca6e8bed82c08c0c416d4348080790a95
hrishav77 <f20210629@goa.bits-pilani.ac.in>
2023-07-08 19:17:44 +0530

    Login
    Made the login method and tested it with postman

--- Backend/Controller/usercontroller.js
+++ Backend/Controller/usercontroller.js
@@ -1,7 +1,21 @@
 const User= require("../Models/User")
+const jwt=require("jsonwebtoken")
 
+const createTokens=(_id)=>{
+ return jwt.sign({_id},process.env.SECRET,{expiresIn:"30d"})
+}
 const loginUser=async(req,res)=>{
-   res.json({msg:"logged in"})
+    const {email,password}=req.body
+    //creating a token for the user
+    
+    try{
+        const user=await User.login(email,password)
+        const token=createTokens(user._id)
+        res.status(200).json({email,token})
+    }catch(error){
+        res.status(400).json({error:error.message})
+    }
+
 
 }
 
@@ -10,11 +24,12 @@ const signupUser=async(req,res)=>{
     
     try{
         const user=await User.signup(email,password)
-        res.json({email,user})
+        const token=createTokens(user._id)
+        res.json({email,token})
     }catch(error){
         res.status(400).json({error:error.message})
     }
 
     }
-
+    
 module.exports={loginUser,signupUser};
--- Backend/Models/User.js
+++ Backend/Models/User.js
@@ -1,5 +1,6 @@
 const mongoose=require('mongoose')
 const bcrypt=require("bcrypt")
+const validator=require("validator")
 const userSchema=new mongoose.Schema({
     email:{
         type:String,
@@ -33,6 +34,21 @@ const hash=await bcrypt.hash(password,salt)
 
 const user=await this.create({email,password:hash})//this means the model
 return user;
+}
+
+userSchema.statics.login=async function(email,password){
+    if(!email||!password){
+        throw Error("all fields must be filled")
+     }   
+     const user=await this.findOne({email})
+     if(!user){
+        throw Error("incorrect emaail")
+     }
+     const match=await bcrypt.compare(password,user.password)
+     if(!match){
+        throw Error("incorrect password")
+     }
+     return user;
 }
 
 module.exports=mongoose.model("User",userSchema)
